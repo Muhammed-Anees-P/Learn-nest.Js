7ab7df0b33cc84a1f3d0b5dcfae6095ff0581965
Muhammed-Anees-P <aneespengad4447@gmail.com>
2025-01-13 11:44:11 +0530

    final commit

--- src/auth/jwt_strategy.ts
+++ src/auth/jwt_strategy.ts
@@ -1,31 +1,28 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { PassportStrategy } from "@nestjs/passport";
-import { Model } from "mongoose";
-import { use } from "passport";
-import { Strategy , ExtractJwt} from "passport-jwt";
-import { user } from "src/book/schemas/user.schema";
-
-
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { PassportStrategy } from '@nestjs/passport';
+import { Model } from 'mongoose';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { user } from 'src/book/schemas/user.schema';
 
 @Injectable()
 export class jwtStrategy extends PassportStrategy(Strategy) {
-    constructor(
-        @InjectModel(user.name)
-        private userModel: Model <user>
-    ){
-        super({
-             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-             secretOrKey: process.env.JWT_SECRET
-        })
+  constructor(
+    @InjectModel(user.name)
+    private userModel: Model<user>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+  async validate(payload) {
+    const { id } = payload;
+    const user = await this.userModel.findById(id);
+    if (!user) {
+      throw new UnauthorizedException('Please login first');
     }
-    async validate(payload){
-        const {id} = payload;
-        const user = await this.userModel.findById(id)
-        if(!user){
-            throw new UnauthorizedException("Please login first")
-        }
 
-        return user;
-    }
+    return user;
+  }
 }
